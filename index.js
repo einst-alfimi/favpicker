7b0e22c6bd947a843a826538a5e1ad3ef0e21871
liquid <einst.4lchemie@gmail.com>
2017-12-05 20:45:03 +0900

    reconnection on error

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
+//import {format, compareAsc} from 'date-fns/esm'
 const Twitter = require('twitter');
 const https = require('https');
-const fs = require('fs');
+const fs = require('fs-extra');
+const date = require('date-fns');
+const EventEmitter = require('events').EventEmitter;
+
 const ConfigFile = require('config');
 
 const client = new Twitter(ConfigFile.twitter);
-client.stream('user', function(stream) {
-    stream.on('favorite', function(event) {
-        client.get('statuses/show/'+event.target_object.id_str, function(error, tweets, response) {
-            if(!tweets){
-                return;
-            }
-            if(!!tweets.extended_entities){
-                //メディアの処理
-                tweets.extended_entities.media.forEach((media) => {
-                    downloadFile(media);
-                    console.log('save images. tweetid:'+tweets.id_str);
-                });
-
-            }else if(!!tweets.entities.media){
-                //メディアの処理
-                downloadFile(tweets.entities.media);
-                console.log('save image. tweetid:'+tweets.id_str);
-            };
+const ev = new EventEmitter;
+ev.on('connect',() => {
+    client.stream('user', function(stream) {
+        stream.on('favorite', function(event) {
+            client.get('statuses/show/'+event.target_object.id_str, function(error, tweets, response) {
+                if(!tweets){
+                    return;
+                }
+                if(!!tweets.extended_entities){
+                    //メディアの処理
+                    tweets.extended_entities.media.forEach((media) => {
+                        downloadFile(media);
+                        console.log('save images. tweetid:'+tweets.id_str);
+                    });
+    
+                }else if(!!tweets.entities.media){
+                    //メディアの処理
+                    downloadFile(tweets.entities.media);
+                    console.log('save image. tweetid:'+tweets.id_str);
+                };
+            });
         });
-    });
-    stream.on('error', function(error) {
-      throw error;
-    });
+        stream.on('error', function(error) {
+            console.error(error);
+            setTimeout(function () {
+                ev.emit('connect');
+            }, 30*1000);
+        });
+    });    
 });
+ev.emit('connect');
 
 function downloadFile(media){
     const url = media.media_url_https;
